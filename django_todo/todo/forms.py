370f476687b640866195a4a9696148f978e51c42
Emre USTA <emre.usta@stu.fsm.edu.tr>
2020-05-22 23:30:11 +0300

    Exercise

---
+++ django_todo/todo/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class TodoForm(forms.Form):
+
+    text = forms.CharField(max_length=40,widget = forms.TextInput(attrs={'class':'form-control','placeholder':'Enter todo e.g.'}))
---
+++ django_todo/todo/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from .models import Todo
+from .forms import TodoForm
+import random
+# Create your views here.
+
+
+def index(request):
+
+    todo_list = Todo.objects.order_by('id')
+
+    form = TodoForm()
+    context = {'todo_list': todo_list, 'form': form}
+    return render(request, 'todo/index.html', context)
+
+
+@require_POST
+def addTodo(request):
+    form = TodoForm(request.POST)
+
+    if form.is_valid():
+        new_todo = Todo(quest=request.POST['text'])
+        new_todo.save()
+    return redirect('index')
+
+
+def completeTodo(request, todo_id):
+
+    todo = Todo.objects.get(pk=todo_id)
+    todo.isComplete = True
+    todo.save()
+
+    return redirect('index')
+
+
+def deleteCompleted(request):
+    Todo.objects.filter(isComplete__exact=True).delete()
+
+    return redirect('index')
+
+
+def deleteAll(request):
+    Todo.objects.all().delete()
+    return redirect('index')
+
+def rndQuest(request):
+    
+    todos = ['Ödevi bitir','Quizi yap','Finale hazırlan','Projeyi bitir','Exercise tamamla']
+    new = Todo(quest =random.choice(todos))
+    new.save()
+    return redirect('index')
